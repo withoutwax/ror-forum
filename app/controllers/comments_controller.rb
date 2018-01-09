3d16ec03f81e6dfacca41d079bff3b299c30a0a3
Will Kim <rlagmlckd@gmail.com>
2018-01-09 02:33:22 -0600

    Model Associations & Basic structure for index and create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params.merge(user_id: current_user.id))
+
+    if @comment.save
+      redirect_to post_path(@post)
+    end
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109005730) do
+ActiveRecord::Schema.define(version: 20180109080009) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|

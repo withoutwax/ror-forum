ca7832eb183e89ca6a975114cd0b7217a86830ff
Will Kim <rlagmlckd@gmail.com>
2018-01-08 19:05:26 -0600

    Basic function to add posts with User associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,30 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.all.order('created_at DESC')
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109004227) do
+ActiveRecord::Schema.define(version: 20180109005730) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180109004227) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
